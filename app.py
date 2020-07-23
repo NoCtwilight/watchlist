cbc6ba20d074ed9953c57c1b6d8b0c1208751c6b
xihuiyan <xihuiyan@qq.com>
2020-07-23 20:48:06 +0800

    Add index page

--- app.py
+++ app.py
@@ -5,12 +5,38 @@ Created on Thu Jul 23 19:27:43 2020
 @author: 23182
 """
 
-from flask import Flask
+from flask import Flask, render_template
+
+
+name = "NoCtwilight"
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
 
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/greet')
 def hello():
     return "欢迎来到资源聚合网页！"
+
+@app.route('/')
+def index():
+    return render_template('index.html', name=name, movies=movies)
+
+
+
+
+
+
+
